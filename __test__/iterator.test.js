81c2b888948a82ee2dd2007e96f66fbef2a0973d
ghd64845 <fleuve1008@gmail.com>
2024-08-22 14:06:38 +0900

    jest setup

---
+++ __test__/iterator.test.js
@@ -0,0 +1,8 @@
+describe("", () => {
+    const a = 1, b = 1
+    test('1 + 1은 2다.', () => {
+        expect(a + b).toEqual(2);
+    })
+
+    
+})
---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.expors = {
+    rootDir: './',
+    testRegex: "\\.test\\.js$",
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8"
+}
